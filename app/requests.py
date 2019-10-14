bcb475215d69fb10e1067895eb82324f3065a866
jecinta <jecintawanjirug@gmail.com>
2019-10-14 12:48:02 +0300

    thus imports the json file and urllib.request

--- app/requests.py
+++ app/requests.py
@@ -9,11 +9,11 @@ api_key = None
 base_url = None
 
 
-def configure_request(app):
+def configure_request(app)
     global api_key, base_url
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_API_BASE_URL']
-
+    
 def get_sources(source):
     """
     Function to retrieve news sources list from the News api
