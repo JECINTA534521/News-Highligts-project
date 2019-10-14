0c9dbe261d30b37bd48a155b9bdb0eeec0fb6d96
jecinta <jecintawanjirug@gmail.com>
2019-10-14 12:05:06 +0300

    this adds the news api key and the base url

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+
+    NEWS_API_KEY = '40a81ce19baa427bb0bfecd3e66b647e'
+    NEWS_API_BASE_URL = 'https://newsapi.org/v1/sources/'
+
+
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
