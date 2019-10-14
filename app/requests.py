a68d0e47f4cbc74be96f13114622561b779108ba
jecinta <jecintawanjirug@gmail.com>
2019-10-14 12:56:30 +0300

    this adds a function that process the results and transforms them to a list of objectives

--- app/requests.py
+++ app/requests.py
@@ -13,7 +13,7 @@ def configure_request(app)
     global api_key, base_url
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_API_BASE_URL']
-    
+
 def get_sources(source):
     """
     Function to retrieve news sources list from the News api
@@ -36,11 +36,12 @@ def get_sources(source):
 
 
 def process_results(sources_list):
-    """Function that process the results list and transforms them into a list of objects
-    Args: sources_list: A list of dictionaries that contains news sources details
+    '''
+    Function that process the results list and transforms them into a list of objects
+    Args: sources_list:A list of dictionaries that contains news sources details
     Returns:
-    sources_results: a list of news sources objects"""
-
+    sources_results: a list of news sources objects
+    '''
     sources_results = []
     for source_item in sources_list:
         id = source_item.get('id')
