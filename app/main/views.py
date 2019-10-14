88df8314e8e931f2219103307983bcae439c3c82
jecinta <jecintawanjirug@gmail.com>
2019-10-14 14:46:53 +0300

    this adds a view root page function that returns index page and the various news sources

--- app/main/views.py
+++ app/main/views.py
@@ -5,12 +5,13 @@ from ..requests import get_sources,get_articles
 
 @main.route('/')
 def index():
-    """View root page function that returns index page and the various news sources"""
-    title = 'Home- Welcome News Highlights Application'
-    # Getting the news sources
+    '''
+    View root page function that returns index page and the various news sources
+    '''
+    title = 'Home- Welcome to News Highlights Application'
+    #Getting the news sources
     news_sources = get_sources('sources')
-    return render_template('index.html', title=title, sources=news_sources)
-
+    return render_template('index.html', title=title,sources=news_sources)
 @main.route('/articles/<source_id>')
 def articles(source_id):
     '''
