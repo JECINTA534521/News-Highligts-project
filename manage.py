00634f8eb0fc972d28f1ec911b83fbdea838ebd5
jecinta <jecintawanjirug@gmail.com>
2019-10-17 15:51:13 +0300

    this tests the tests whether they are working

--- manage.py
+++ manage.py
@@ -6,13 +6,12 @@ app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server', Server)
-
-
+@manager.command
 def test():
     """ Run the unit tests. """
     import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+    test = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner(verbosity=2).run(test)
 
 
 if __name__ == '__main__':
